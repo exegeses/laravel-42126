2c40b7bb63d02240a4ae022a32bdd3c997de66f3
marcos pinardi <exegeses@gmail.com>
2020-11-17 12:04:10 -0300

    teoría y creaciónde MVC

---
+++ catalogo/app/Http/Controllers/TestController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    //index
+    public function listarMarcas()
+    {
+
+    }
+
+    //show
+    public function vermarcaPorID()
+    {
+
+    }
+
+    //create & store
+    public function agregarMarca()
+    {
+
+    }
+    //edit & update
+}
