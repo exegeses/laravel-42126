c435e96afdddaeff51dbff5810bdd281ddac10a5
marcos pinardi <exegeses@gmail.com>
2020-11-26 12:13:11 -0300

    método validate() en ProductoController

--- catalogo/app/Http/Controllers/ProductoController.php
+++ catalogo/app/Http/Controllers/ProductoController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categoria;
+use App\Models\Marca;
 use App\Models\Producto;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 
 class ProductoController extends Controller
@@ -30,11 +31,48 @@ class ProductoController extends Controller
      */
     public function create()
     {
-        //??
+        //obtener listados de marcas y categorias
+        $marcas = Marca::all();
+        $categorias = Categoria::all();
 
-        //retornamos vista del form
-        return view('/agregarProducto');
+        //retornamos vista del form con los listados
+        return view('/agregarProducto',
+                    [
+                        'marcas'=>$marcas,
+                        'categorias'=>$categorias
+                    ]
+                );
     }
+
+    private function validar(Request $request)
+    {
+        $request->validate(
+            [
+                'prdNombre'=>'required|min:3|max:70',
+                'prdPrecio'=>'required|numeric|min:0',
+                'prdPresentacion'=>'required|min:3|max:150',
+                'prdStock'=>'required|integer|min:1',
+                'prdImagen'=>'mimes:jpg,jpeg,png,gif,svg,webp|max:2048'
+            ],
+            [
+                'prdNombre.required'=>'Complete el campo Nombre',
+                'prdNombre.min'=>'Complete el campo Nombre con al menos 3 caractéres',
+                'prdNombre.max'=>'Complete el campo Nombre con 70 caractéres como máxino',
+                'prdPrecio.required'=>'Complete el campo Precio',
+                'prdPrecio.numeric'=>'Complete el campo Precio con un número',
+                'prdPrecio.min'=>'Complete el campo Precio con un número positivo',
+                'prdPresentacion.required'=>'Complete el campo Presentación',
+                'prdPresentacion.min'=>'Complete el campo Presentación con al menos 3 caractéres',
+                'prdPresentacion.max'=>'Complete el campo Presentación con 150 caractérescomo máxino',
+                'prdStock.required'=>'Complete el campo Stock',
+                'prdStock.integer'=>'Complete el campo Stock con un número entero',
+                'prdStock.min'=>'Complete el campo Stock con un número positivo',
+                'prdImagen.mimes'=>'Debe ser una imagen',
+                'prdImagen.max'=>'Debe ser una imagen de 2MB como máximo'
+            ]
+        );
+    }
+
 
     /**
      * Store a newly created resource in storage.
@@ -44,7 +82,11 @@ class ProductoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //validación
+
+        //instancias y guardar
+
+        //redirigir con mensaje de ok
     }
 
     /**
