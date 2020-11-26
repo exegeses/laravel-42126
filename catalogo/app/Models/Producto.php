fc73a37abda9b4f8a64d14e3ea7ed6c2b3dc5dc4
marcos pinardi <exegeses@gmail.com>
2020-11-26 13:07:12 -0300

    aplicación de alta de un producto

--- catalogo/app/Models/Producto.php
+++ catalogo/app/Models/Producto.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Producto extends Model
 {
     use HasFactory;
+    protected $primaryKey = 'idProducto';
+    public $timestamps = false;
 
     #relación a tabla marcas
     public function relMarca()
